e3c54e8f54f98b91e5318cabf4e6c8a140738625
Michael Ralea <mralea99@gmail.com>
2021-03-24 13:31:35 -0400

    Remove preliminary handshaking

--- SDR_TestFiles/stdin_testing.py
+++ SDR_TestFiles/stdin_testing.py
@@ -5,17 +5,12 @@ GPIO.setmode(GPIO.BCM)
 
 # Set variable names for GPIO pin numbers
 data_pins = [27, 28, 3, 5, 7, 29, 31, 26]
-clock_pin = 24
-chip_select = 21
 
 GPIO.setup(clock_pin, GPIO.OUT)
 GPIO.setup(chip_select, GPIO.OUT)
 
 # Send a single byte of data out to the FPGA
 def send_byte(byte_out):
-    GPIO.output(clock_pin, 0)
-    GPIO.output(chip_select, 1)     # High means writing, low will mean reading
-
     # Send the byte
     GPIO.setup(data_pins, GPIO.OUT)
     GPIO.output(data_pins, byte_out)
@@ -29,7 +24,7 @@ try:
     # Control array to be updated by the data coming from the SDR, then 
     # passed to the send_byte function to be output to the data_pins
     control_array = [0, 0 , 0, 0, 0, 0 , 0, 0]
-    for data in sys.stdin.buffer.read(1):
+    for data in sys.stdin.buffer.read(16):
 
         data = data - 128                   # Data is of type int read in as uint8, subtract 128
         data_bin = bin(data & 0b11111111)   # Convert to 2's complement
