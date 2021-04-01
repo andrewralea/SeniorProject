79a370c3485708078dc39cd1bc9edafc66a1d835
Michael Ralea <mralea99@gmail.com>
2021-04-01 11:37:18 -0400

    Testing GPIO, details in Issues

--- SDR_TestFiles/PMOD_Test.py
+++ SDR_TestFiles/PMOD_Test.py
@@ -6,24 +6,25 @@ GPIO.setmode(GPIO.BCM)
 
 # Set data pins to be used for output, connected to
 # PMOD ports on Nexys 4 DDR FPGA
-data_pins = [1, 2, 3, 4, 5, 6, 7, 8]
+# data_pins = [3, 4, 17, 27, 22, 10, 9, 2]
+data_pins = 2
 GPIO.setup(data_pins, GPIO.OUT)
 
 try:
     # For num runs, switch the output pins from 
     # high to low every 2 seconds, outputting to screen for
     # verification
-    num_runs = 5
+    num_runs = 2
     for x in range(num_runs):
-        GPIO.output(data_pins, [1, 1, 1, 1, 1, 1, 1, 1])
-        print('LED ON')
-        sleep(2)
-        GPIO.output(data_pins, [0, 0, 0, 0, 0, 0, 0, 0])
+        GPIO.output(data_pins, 0)
         print('LED OFF')
+        sleep(2)
+        GPIO.output(data_pins, 1)
+        print('LED ON')
         sleep(2)
 
 except KeyboardInterrupt:
-    print("User exited with CTRL+C")
+    print("\nUser exited with CTRL+C")
 
 finally:
     GPIO.cleanup()                          # Clear GPIO pins so other programs can use them
