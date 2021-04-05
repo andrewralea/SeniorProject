693faf6b7aebbec8c8e6c62c0b6aef01a3203fdb
Michael Ralea <mralea99@gmail.com>
2021-04-05 16:43:58 -0400

    Begin progress on C conversion

--- SDR_TestFiles/Handshaking_Translate.c
+++ SDR_TestFiles/Handshaking_Translate.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 
 int main() {
-    char buf[1];
-    while(read(0, buf, sizeof(buf)) > 0) {
-        fprintf(stdout, buf)
+    char buf[1];                            // read 4 bytes
+    unsigned int byte_in;
+
+    freopen(0, "rb", stdin);                // open stdin in binary mode
+    while(read(0, buf, sizeof(buf)) > 0) {  // stdin file descriptor = 0
+        byte = (unsigned int) buf;
+        fprintf(stdout, byte);
     }
+    fclose(stdin);
 }
--- SDR_TestFiles/sdr_parallel.sh
+++ SDR_TestFiles/sdr_parallel.sh
@@ -2,6 +2,8 @@
 
 # Run 2 SDRs and their GPIO control programs at the same time
 # Identify which SDR by the -d parameter
-#rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 stdin_testing.py &
+# rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 stdin_testing.py &
 # rtl_sdr -f 99000000 -g 40 -s 2500000 -d 1 - |  &
-rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 Handshaking_Test.py
+# rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 Handshaking_Test.py
+
+rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | ./Handshaking_Translate
