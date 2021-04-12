4b95dcdc450775dc2fe00bef0fbafd78f5a64a0c
Michael Ralea <mralea99@gmail.com>
2021-04-12 12:21:08 -0400

    Update c code to read in values correctly

--- SDR_TestFiles/Handshaking_Translate.c
+++ SDR_TestFiles/Handshaking_Translate.c
@@ -3,22 +3,26 @@
 #include <stdlib.h>
 
 int main() {
-    char buf[1];                          
-    int byte_in;
-    int rv;
+    const unsigned int num_bytes = 1;           // num bytes to be read
+    unsigned char data_in_buf[num_bytes];       // to read data in    
+    signed char signed_data_buf[num_bytes];     // to hold converted data
+    int data_in;
+    int counter = 0;
 
-    freopen(NULL, "rb", stdin);                // open stdin in binary mode
+    freopen(NULL, "rb", stdin);                 // open stdin in binary mode
     do {
-        rv = read(0, buf, sizeof(buf));     // try to read a full buffer
-        if (rv > 0) {
-            for (int i = 0; i < rv; ++i) {
-                fputc(buf[i], stdout);
+        data_in = read(0, data_in_buf, sizeof(data_in_buf));     // try to read a full buffer
+        if (data_in > 0) {                                       // if there is data
+            for (int i = 0; i < data_in; ++i) {
+                signed_data_buf[i] = data_in_buf[i] - 128;
+                printf("%d\n", signed_data_buf[i]);
             }
         }
         else {
             printf("Error reading full buffer");
         }
-    } while (rv > 0);
+        counter = counter + 1;
+    } while (counter < 128);
 
     fclose(stdin);
 }
