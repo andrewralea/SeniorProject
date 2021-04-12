f90c416e45476b400d132465077a575cd58a68ae
Michael Ralea <mralea99@gmail.com>
2021-04-12 12:49:57 -0400

    Create GPIO testing for C code, minor updates for stdin reading

--- SDR_TestFiles/Handshaking_Translate.c
+++ SDR_TestFiles/Handshaking_Translate.c
@@ -6,14 +6,16 @@ int main() {
     const unsigned int num_bytes = 1;           // num bytes to be read
     unsigned char data_in_buf[num_bytes];       // to read data in    
     signed char signed_data_buf[num_bytes];     // to hold converted data
+
     int data_in;
     int counter = 0;
+    int i;
 
     freopen(NULL, "rb", stdin);                 // open stdin in binary mode
     do {
-        data_in = read(0, data_in_buf, sizeof(data_in_buf));     // try to read a full buffer
+        data_in = read(0, data_in_buf, sizeof(data_in_buf));     // try to read a full buffer from stdin
         if (data_in > 0) {                                       // if there is data
-            for (int i = 0; i < data_in; ++i) {
+            for (i = 0; i < data_in; ++i) {
                 signed_data_buf[i] = data_in_buf[i] - 128;
                 printf("%d\n", signed_data_buf[i]);
             }
