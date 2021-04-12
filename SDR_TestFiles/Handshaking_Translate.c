fd1f20d2a01b801c46495df737b123f150c68f63
Michael Ralea <mralea99@gmail.com>
2021-04-12 10:48:58 -0400

    Update c code to handle error, read bytes in continuously

--- SDR_TestFiles/Handshaking_Translate.c
+++ SDR_TestFiles/Handshaking_Translate.c
@@ -3,13 +3,22 @@
 #include <stdlib.h>
 
 int main() {
-    char buf[1];                            // read 4 bytes
+    char buf[1];                          
     int byte_in;
+    int rv;
 
     freopen(NULL, "rb", stdin);                // open stdin in binary mode
-    while(read(0, buf, sizeof(buf)) > 0) {  // stdin file descriptor = 0
-        byte_in = (int) buf;
-        fprintf(stdout, "%d", byte_in);
-    }
+    do {
+        rv = read(0, buf, sizeof(buf));     // try to read a full buffer
+        if (rv > 0) {
+            for (int i = 0; i < rv; ++i) {
+                fputc(buf[i], stdout);
+            }
+        }
+        else {
+            printf("Error reading full buffer");
+        }
+    } while (rv > 0);
+
     fclose(stdin);
 }
--- SDR_TestFiles/sdr_parallel.sh
+++ SDR_TestFiles/sdr_parallel.sh
@@ -4,6 +4,6 @@
 # Identify which SDR by the -d parameter
 # rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 stdin_testing.py &
 # rtl_sdr -f 99000000 -g 40 -s 2500000 -d 1 - |  &
- rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 Handshaking_Test.py
+# rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | python3 Handshaking_Test.py
 
-# rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | ./Handshaking_Translate
+rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | ./Handshaking_Translate
