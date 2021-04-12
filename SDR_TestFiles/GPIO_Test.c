bde187d038ba0ae7fb246ef761f61b72b29fc05c
Michael Ralea <mralea99@gmail.com>
2021-04-12 13:17:34 -0400

    Confirm gpio manipulation in C

--- SDR_TestFiles/GPIO_Test.c
+++ SDR_TestFiles/GPIO_Test.c
@@ -1,7 +1,26 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pigpio.h>
 
-int main() {
-    printf("To be implemented\n");
+int main(int argc, char *argv[]) {
+    double start;
+
+    if (gpioInitialise() < 0) {
+        fprintf(stderr, "pigpio init failed\n");
+        return 1;
+    }
+
+    gpioSetMode(3, PI_OUTPUT);
+
+    start = time_time();
+
+    while ((time_time() - start) < 5) {
+        gpioWrite(3, 1); // on
+        time_sleep(1);
+        gpioWrite(3, 0);
+        time_sleep(1);
+    }
+
+    gpioTerminate();
 }
