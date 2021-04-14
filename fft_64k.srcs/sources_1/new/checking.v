b165d64416370b0640a932c02ff7bbefb09327bb
krupatishbi <72935073+krupatishbi@users.noreply.github.com>
2021-04-14 17:41:47 -0400

    Add files via upload
    maybe working write/read to ram for checking

---
+++ fft_64k.srcs/sources_1/new/checking.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/14/2021 01:29:21 PM
+// Design Name: 
+// Module Name: checking
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module checking(
+    input clk,
+    input [7:0] data_in,
+    output full,
+    output reg [7:0] data_out
+    );
+    
+    reg count = 0;
+    reg [15:0] read_addr, write_addr;
+    reg [1:0] wea;
+    reg [15:0] d_ina, d_outb;
+    reg [15:0] fulldata;
+    
+    initial
+    begin
+       wea =  2'b00;
+       write_addr = 0;
+       read_addr = 0;
+       count = 0;
+    end
+    
+    always @ (posedge clk)
+    begin
+        if(count < 2)
+            count <= count + 1;
+        else
+            count <= 0;
+            
+        
+        if(write_addr < 65536)
+        begin
+            d_ina <= {data_in, data_in};   
+            write_addr <= write_addr + 1;
+            if(count == 0)
+                wea <= 2'b01;
+            else
+                wea <= 2'b10;
+        end
+        else
+        begin
+            wea <= 2'b00;
+            
+            if(read_addr < 65536)
+            begin
+                if(count  == 0)
+                    data_out <= d_outb[7:0];
+                else
+                begin
+                    data_out <= d_outb[15:8];
+                    read_addr <= read_addr + 1;
+                end
+            end
+        end
+    end
+    
+    
+    
+    blk_mem_gen_0 check (.clka(clk), .wea(wea), .addra(write_addr), .dina(d_ina), .clkb(clk)
+    , .addrb(read_addr), .doutb(d_outb));
+    
+endmodule
