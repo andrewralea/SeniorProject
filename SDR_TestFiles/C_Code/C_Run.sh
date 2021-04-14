131658ce1c44dbcfe4f0db45c09a1a109130fe51
Michael Ralea <mralea99@gmail.com>
2021-04-14 12:46:31 -0400

    Add timing testing

--- SDR_TestFiles/C_Code/C_Run.sh
+++ SDR_TestFiles/C_Code/C_Run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | sudo ./main_control
+#rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | sudo ./main_control
+rtl_sdr -f 99000000 -g 40 -s 2500000 -d 0 - | sudo ./timing_testing
