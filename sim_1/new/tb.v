f46150c3d62764a548b29ea371a664bda0c6b841
krupatishbi <72935073+krupatishbi@users.noreply.github.com>
2021-04-15 16:41:23 -0400

    Add files via upload

--- sim_1/new/tb.v
+++ sim_1/new/tb.v
@@ -40,15 +40,28 @@ always #5 clk = ~clk;
 wire [14:0] bfly_index;
 wire [14:0] bunch_index;
 
+reg [7:0] data_out;
+reg [7:0] out;
+reg full;
+
 
 initial
 begin
     reset =  1;
     #100
     reset  = 0;
+    out  = 8'b0;
 end
 
-ab_selector UUT (clk, reset, 1, 1, 4, 1, 1, bfly_index, bunch_index);
+always @ (posedge clk)
+begin
+    if (out < 131072)
+        out <= out + 1;
+end
+
+checking UUT (clk, out, full, data_out);
+
+//ab_selector UUT (clk, reset, 1, 1, 4, 1, 1, bfly_index, bunch_index);
 
 //complex_butterfly  UUT (clk, reset, a_in, b_in, weights, a_out, b_out);
 
