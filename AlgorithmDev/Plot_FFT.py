829af6173fa84c23088505ed59bb55a86cf463ac
Michael Ralea <mralea99@gmail.com>
2021-04-26 14:50:48 -0400

    Read from file

--- AlgorithmDev/Plot_FFT.py
+++ AlgorithmDev/Plot_FFT.py
@@ -1,27 +1,20 @@
-import numpy as np
-import matplotlib.pyplot as plt # library for plotting
-from signalgen import sine_wave # import the function
-from scipy.fftpack import fft, ifft
+# Michael Ralea and Krupa Tishbi
 
-def sine_wave(f,overSampRate,phase,nCyl):
-	fs = overSampRate*f # sampling frequency
-	t = np.arange(0,nCyl*1/f-1/fs,1/fs) # time base
-	g = np.sin(2*np.pi*f*t+phase) # replace with cos if a cosine wave is desired
-	return (t,g) # return time base and signal g(t) as tuple
+try:
+	counter = 0
+	# Open .dat file of SDR captured data
+	filename = "FMCapture_2.dat"
+	f = open(filename, "rb")
+	while True:
+		for data in f.read(4):
+			if data == '':
+				break
+			print(data)
+			counter += 1
 
-f = 10 #frequency = 10 Hz
-overSampRate = 30 #oversammpling rate
-fs = f*overSampRate #sampling frequency
-phase = 1/3*np.pi #phase shift in radians
-nCyl = 5 # desired number of cycles of the sine wave
+except KeyboardInterrupt:
+    print("User exited with CTRL+C")
 
-(t,x) = sine_wave(f,overSampRate,phase,nCyl) #function call
-
-plt.plot(t,x) # plot using pyplot library from matplotlib package
-plt.title('Sine wave f='+str(f)+' Hz') # plot title
-plt.xlabel('Time (s)') # x-axis label
-plt.ylabel('Amplitude') # y-axis label
-plt.show() # display the figure
-
-X = fft(x,N) #compute X[k]
-x = ifft(X,N) #compute x[n]
+finally:
+	f.close()
+	print(counter)
