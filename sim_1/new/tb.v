0363e2c1cab7dc6135e2dbd5d2573da8f7171da5
krupatishbi <72935073+krupatishbi@users.noreply.github.com>
2021-04-26 18:53:52 -0400

    testing checking module
    ahhhhhhh

--- sim_1/new/tb.v
+++ sim_1/new/tb.v
@@ -37,32 +37,46 @@ reg [7:0] weights[1:0];
 
 always #5 clk = ~clk;
 
-wire [14:0] bfly_index;
-wire [14:0] bunch_index;
-
 reg [7:0] data_out;
 reg [7:0] out;
-reg full;
+reg full, out_rtr, in_rts;
+reg [15:0] count;
 
+reg [14:0] a_start, ab_offset, max_bfly_index, max_bunch_index, num_bunch;
+wire [14:0] bfly_index, bunch_index;
 
 initial
 begin
     reset =  1;
     #100
     reset  = 0;
-    out  = 8'b0;
+    count  = 8'b0;
+    in_rts = 1'b0;
+//    a_start = 15'b000000000000000;
+//    ab_offset = 15'b000000000010000;
+//    max_bfly_index = 15'b000000000100000;
+//    max_bunch_index = 15'b000000000100000;
+//    num_bunch = 15'b000000000000001;
 end
 
 always @ (posedge clk)
 begin
-    if (out < 131072)
-        out <= out + 1;
+    if (count < 131071)
+        count <= count + 1;        
 end
 
-checking UUT (clk, out, full, data_out);
+assign out = count;
 
-//ab_selector UUT (clk, reset, 1, 1, 4, 1, 1, bfly_index, bunch_index);
+always #50 in_rts = ~in_rts;
 
+//top UUT(clk, reset, out, in_rts, full, out_rtr);
+
+checking UUT (clk, out, in_rts, full, data_out, out_rtr);
+
+
+
+
+//ab_selector UUT (clk, reset, a_start, ab_offset, max_bfly_index, max_bunch_index, num_bunch, bfly_index, bunch_index);
 //complex_butterfly  UUT (clk, reset, a_in, b_in, weights, a_out, b_out);
 
 endmodule
