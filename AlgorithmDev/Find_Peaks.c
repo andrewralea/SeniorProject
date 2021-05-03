393223867cf178ae644e5a60b8fcd14d9df0d82f
Michael Ralea <mralea99@gmail.com>
2021-05-03 17:29:11 -0400

    Supply stimulus, C not working yet

--- AlgorithmDev/Find_Peaks.c
+++ AlgorithmDev/Find_Peaks.c
@@ -15,22 +15,39 @@ int main() {
     printf("Max signal width (in num bins): %i\n", peak_margin);
     printf("Max number of peaks: %i\n", max_num_peaks);
 
+
+    /* ---------------------------------------------------------------- */
+    /*    Open a file for read that stores PSD data to verify algo      */
+    /* ---------------------------------------------------------------- */
+    FILE *f_in = fopen("PowerData.bin", "rb");    
+    if (f_in == NULL) {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    for (int k = 0; k < FFT_PointSize; k++) {
+        fscanf(f_in, "%0.3f", PSD[k]);
+        //printf("%0.3f", PSD[k]);
+    }
+    /* ---------------------------------------------------------------- */
+
     float temp_max;     // temporary max for finding peak power value
     int temp_loc;       // to store bin number of temp_max
     int loc_exists;     // flag to indicate a location already exists
-    for (int cur_peak_bin = 1; cur_peak_bin < max_num_peaks; cur_peak_bin++) {
+    int cur_peak_bin;
+    for (cur_peak_bin = 0; cur_peak_bin < max_num_peaks; cur_peak_bin++) {
         temp_max = 0.0;
         temp_loc = 0;
 
         /* Iterate through all data points */
-        for (int i = 1; i < 32768; i++) {
+        for (int i = 0; i < 32768; i++) {
             loc_exists = 0;
             /*  Check if value at current index is over threshold
                 and also a new maximum */
             if (PSD[i] > temp_max && PSD[i] > peak_threshold) {
                 /*  Compare this max's index against existing ones. 
                     Also make sure it's not too close to an existing */
-                for (int j = 1; j < cur_peak_bin; j++) {
+                for (int j = 0; j < cur_peak_bin; j++) {
                     if (i > (peak_locs[j] - peak_margin / 2) && i < (peak_locs[j] + peak_margin / 2)) {
                         loc_exists = 1;
                         break;
@@ -49,4 +66,10 @@ int main() {
             peak_locs[cur_peak_bin] = temp_loc;
         }
     }
+
+    for (int i = 1; i < cur_peak_bin - 1; i++) {
+        printf("Peak %d\n", i);
+        printf("Bin = %d\n", peak_locs[i]);
+    }
+    fclose(f_in);
 }
